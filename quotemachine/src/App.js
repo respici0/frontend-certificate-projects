20a9882f1240bd5b9a4c5129aedbb02097991c53
respici0 <respicio.robert@gmail.com>
2019-03-13 16:18:37 -0700

    added quotes

--- quotemachine/src/App.js
+++ quotemachine/src/App.js
@@ -1,29 +1,79 @@
 import React, { Component } from "react";
 import "./App.scss";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: ""
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
+      .then(this.onGetQuoteSuccess)
+      .catch(this.onGetQuoteError);
+  };
+
+  onGetQuoteSuccess = response => {
+    console.log("Random Quote", response.data.quotes);
+  };
+
+  onGetQuoteError = response => {
+    console.log("Quote Error", response);
+  };
+
+  quoteButtonClick = () => {
+    console.log("Quote button clicked");
+  };
+
+  tweetButtonClick = () => {
+    console.log("Tweet button clicked");
+  };
+
   render() {
     return (
-      <div className="App">
-        <div id="quote-box">
-          <div className="row">
+      <React.Fragment>
+        <div className="App">
+          <div id="quote-box">
             <div id="text">
               <p>"Random quote that will be dynamic soon"</p>
             </div>
             <div id="author">
               <p>-Author Name</p>
             </div>
-          </div>
-          <div className="row">
-            <button type="button" className="btn btn-success" id="new-quote">
+            <button
+              type="button"
+              className="btn btn-primary"
+              id="tweet-quote"
+              onClick={this.tweetButtonClick}
+            >
+              <i className="fab fa-twitter" />
+            </button>
+            <button
+              type="button"
+              className="btn btn-success"
+              id="new-quote"
+              onClick={this.quoteButtonClick}
+            >
               New quote
             </button>
-            <button type="button" className="btn btn-primary" id="tweet-quote">
-              <i className="fab fa-twitter" /> Tweet quote
-            </button>
+          </div>
+          <div className="footer">
+            <p>
+              Created by{" "}
+              <a href="https://robertpaulrespicio.com/" target="_block">
+                Respici0
+              </a>
+            </p>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
