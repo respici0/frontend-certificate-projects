103fcf1ba3aa73b9356e9ac84c02ca324cc34b47
respici0 <respicio.robert@gmail.com>
2019-03-13 14:09:22 -0700

    work on later

--- quotemachine/src/App.js
+++ quotemachine/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <div id="quote-box">
+          <div className="row">
+            <div id="text">
+              <p>"Random quote that will be dynamic soon"</p>
+            </div>
+            <div id="author">
+              <p>-Author Name</p>
+            </div>
+          </div>
+          <div className="row">
+            <button type="button" className="btn btn-success" id="new-quote">
+              New quote
+            </button>
+            <button type="button" className="btn btn-primary" id="tweet-quote">
+              <i className="fab fa-twitter" /> Tweet quote
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
